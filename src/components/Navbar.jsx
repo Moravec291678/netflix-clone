d1d8b05f153f4c2dfd25025e461fdd664a886507
DavidMoravec <David.Moravec2@seznam.cz>
2026-04-30 19:21:13 +0200

    add structur for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,45 @@
 import { Link } from "react-router-dom";
+import searchIcon from "../images/search-icon.png";
+import notificationIcon from "../images/notification.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 function Navbar({ selectedProfile }) {
   return (
     <header className="header">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/960px-Netflix_2015_logo.svg.png?_=20190206123158"
+        alt=""
+        className="logo"
+      />
       <nav className="nav">
         <ul className="nav-list">
           <li>
-            <Link>Domovská stránka</Link>
+            <Link to={`/home/${selectedProfile.id}`}>Domovská stránka</Link>
           </li>
           <li>
-            <Link>Pořady</Link>
+            <Link to={"/"}>Pořady</Link>
           </li>
           <li>
-            <Link>Filmy</Link>
+            <Link to={"/"}>Filmy</Link>
           </li>
           <li>
-            <Link>Nové a oblíbené</Link>
+            <Link to={"/"}>Nové a oblíbené</Link>
           </li>
           <li>
-            <Link>Můj seznam</Link> 
+            <Link to={"/"}>Můj seznam</Link>
           </li>
         </ul>
       </nav>
+      <div className="header-right">
+        <img className="search-icon" src={searchIcon} alt="search" />
+        <span className="kid-btn">Deti</span>
+
+        <img src={notificationIcon} alt="notification" />
+        <div className="small-profile">
+          <img src={selectedProfile.avatar} alt={selectedProfile.name} />
+          <FontAwesomeIcon icon={faChevronUp} />
+        </div>
+      </div>
     </header>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import { useParams } from "react-router-dom";
 import { profiles } from "../data/profiles";
 import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 
 function Home() {
   const { id } = useParams();
-  const selectedProfile = profiles.find((profile) => profile.id === Number(id));
+  const selectedProfile = profiles.find((profile) => String(profile.id) === id);
+
+  if (!selectedProfile) {
+    return (
+      <>
+        <p>Profil nenalezen</p>
+        <Link to="/">Zpatky na profily</Link>
+      </>
+    );
+  }
 
   return (
     <>
